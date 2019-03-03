9207b6eaf433a37667d5e7db77c0f5744c667eb3
zach_liu <yitachi@gmail.com>
2019-03-03 10:51:13 -0500

    clean up

--- src/containers/MenuContainer/menu.container.js
+++ src/containers/MenuContainer/menu.container.js
@@ -1,23 +1,15 @@
 import React, { Component } from "react";
-import { GridMenu } from "../../components/common/gridMenu/GridMenu";
-import { Text, View, StyleSheet, Dimensions } from "react-native";
-import { createMaterialTopTabNavigator } from "react-navigation";
+import { View } from "react-native";
 import tabs from "./menu.tabs";
-import { actions } from "../../modules/Menu/Menu.actions";
 import { createAppContainer } from "react-navigation";
-const data = [
-  {
-    key: "Bench Press",
-    image:
-      "https://firebasestorage.googleapis.com/v0/b/fitn3ss-3790e.appspot.com/o/exercises%2Fbench_press.jpeg?alt=media&token=5fd9c10e-9db9-4089-93da-f2b3a881d649"
-  }
-];
-// { key: "Chin Up", image: require("../../pic/chin_up.jpeg") },
-// { key: "Shoulder Press", image: require("../../pic/shoulder_press.jpeg") },
-// { key: "Squats", image: require("../../pic/squats.jpeg") },
-// { key: "Dead Lift", image: require("../../pic/dead_lift.jpeg") }
+// const testingData = [
+//   {
+//     key: "Bench Press",
+//     image:
+//       "https://firebasestorage.googleapis.com/v0/b/fitn3ss-3790e.appspot.com/o/exercises%2Fbench_press.jpeg?alt=media&token=5fd9c10e-9db9-4089-93da-f2b3a881d649"
+//   }
+// ];
 
-const numColumns = 2;
 const MenuTabs = createAppContainer(tabs);
 
 class Menu extends Component {
@@ -31,27 +23,3 @@ class Menu extends Component {
 }
 
 export default Menu;
-
-// export class LiftingTab extends Component {
-//   render() {
-//     return <GridMenu items={data} numColumns={numColumns} />;
-//   }
-// }
-
-// export class cardioTab extends Component {
-//   render() {
-//     return (
-//       <View>
-//         <Text>cardio</Text>
-//       </View>
-//     );
-//   }
-// }
-
-// // const cardioTab = Tab(data, numColumns);
-// // const LiftingTab = Tab(data, numColumns);
-
-// export default createMaterialTopTabNavigator({
-//   Lifting: { screen: LiftingTab },
-//   Cardio: { screen: cardioTab }
-// });
--- src/containers/MenuContainer/menu.tabs.js
+++ src/containers/MenuContainer/menu.tabs.js
@@ -3,15 +3,6 @@ import { createAppContainer } from "react-navigation";
 import liftingTab from "./tabs/menu.liftingTab";
 import cardioTab from "./tabs/menu.cardioTab";
 
-// //test tab view ******************************************************************
-const data = [
-  {
-    key: "Bench Press",
-    image:
-      "https://firebasestorage.googleapis.com/v0/b/fitn3ss-3790e.appspot.com/o/exercises%2Fbench_press.jpeg?alt=media&token=5fd9c10e-9db9-4089-93da-f2b3a881d649"
-  }
-];
-
 const tabs = createAppContainer(
   createMaterialTopTabNavigator({
     Lifting: liftingTab,
