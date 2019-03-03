3782dcb3818fbcae3802fc57c7a6854b7c4a67b3
zach_liu <yitachi@gmail.com>
2019-03-03 10:44:45 -0500

    part of the menu

---
+++ src/containers/MenuContainer/tabs/menu.liftingTab.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import _ from "lodash";
+
+import { GridMenu } from "../../../components/common/gridMenu/GridMenu";
+import { connect } from "react-redux";
+import { actions } from "../../../modules/Menu/Menu.actions";
+
+const numColumns = 2;
+var data = [];
+class liftingTab extends Component {
+  componentWillMount() {
+    data = convertExerciseJsonToArray(this.props.exercises);
+    console.log(data);
+  }
+
+  render() {
+    return <GridMenu items={data} numColumns={numColumns} />;
+  }
+}
+
+const convertExerciseJsonToArray = exercises => {
+  var array = [];
+  _.forEach(exercises, function(exercise) {
+    arrayItem = {
+      key: `${exercise.exerciseName}`,
+      image: `${exercise.exerciseMenuPic}`
+    };
+    array.push(arrayItem);
+  });
+
+  return array;
+};
+
+const mapStateToProps = state => {
+  const exercises = _.map(
+    state.menuReducer.exercises.payload.lifting,
+    (exercises, exerciseName) => {
+      return { ...exercises, exerciseName };
+    }
+  );
+
+  return { exercises };
+};
+
+export default connect(
+  mapStateToProps,
+  actions
+)(liftingTab);
